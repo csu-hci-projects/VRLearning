9f0d193418f196496067805f3207e12bd35acaaf
Parker Thompson <pt@rams.colostate.edu>
2020-03-27 06:38:10 -0600

    Added unity project

---
+++ unity/LogicLecture/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+    public GameObject[] cameras;
+
+    void Update(){
+        checkForKeyPress();
+    }
+
+    void checkForKeyPress(){
+        if (Input.GetKeyDown(KeyCode.RightArrow)){
+            changeCamera(1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            changeCamera(-1);
+        }
+    }
+
+    void changeCamera(int change){
+        int currentCamera = PlayerPrefs.GetInt("CameraPosition");
+
+        int lastCamera = currentCamera;
+
+        currentCamera = currentCamera + change;
+
+        if (currentCamera > cameras.Length-1){
+            currentCamera = 0;
+        }
+        if (currentCamera < 0){
+            currentCamera = cameras.Length-1;
+        }
+
+        PlayerPrefs.SetInt("CameraPosition", currentCamera);
+
+        cameras[lastCamera].SetActive(false);
+        cameras[currentCamera].SetActive(true);
+
+    }
+}
---
+++ unity/LogicLecture/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    public GameObject meteor;
+
+    public void OnMouseDown(){
+        spawnObject();
+    }
+
+    void spawnObject(){
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Instantiate(meteor, mousePosition, Quaternion.identity);
+    }
+
+    void Update(){
+
+    }
+}
