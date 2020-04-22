bfd3435459051a72bdbd417f3b127164f2abe90b
BigIslandGabe <gabep@rams.colostate.edu>
2020-04-22 11:59:11 -0600

    Update MultipleChoiceQuiz

--- unity/MultipleChoiceQuiz/Assets/Scripts/GameManager.cs
+++ unity/MultipleChoiceQuiz/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using TMPro;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
@@ -29,9 +30,11 @@ public class GameManager : MonoBehaviour {
     public CanvasGroup Answer;
     public TextMeshProUGUI answerText;
 
-    public static int Score;
+    public static int Score = 0;
+    private static string questionLog = "";
+    private static int count = 1;
+    private int baseScore = 100;
     private float timeBetweenQuestion = 1f;
-    private float multiplier = 1f;
 
     void Start() {
         if (unansweredQuestions == null || unansweredQuestions.Count == 0) {
@@ -41,8 +44,7 @@ public class GameManager : MonoBehaviour {
     }
 
     void GetRandomQuestion() {
-        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
-        currentQuestion = unansweredQuestions[randomQuestionIndex];
+        currentQuestion = unansweredQuestions[0];
         currentCorrectAnswer = currentQuestion.correctAnswer;
         
         if (currentQuestion.question.Length == 0) {
@@ -73,17 +75,25 @@ public class GameManager : MonoBehaviour {
 
     IEnumerator TransitionFinish() {
         Answer.alpha = 1;
+        CreateText();
         yield return new WaitForSeconds(timeBetweenQuestion);
         Application.Quit();
     }
 
     void UserSelectAnswer(int answerIndex) {
         if (currentCorrectAnswer == answerIndex) {
+            GameManager.Score += baseScore;
+            GameManager.questionLog += "Question " + (count++).ToString() + ": CORRECT\n";
+            baseScore = 100;
             answerText.text = "CORRECT";
             Answer.alpha = 1;
             StartCoroutine(TransitionToNextQuestion());
         }
         else {
+            if (baseScore == 100) {
+                GameManager.questionLog += "Question " + (count++).ToString() + ": INCORRECT\n";
+            }
+            baseScore -= 25;
             answerText.text = "INCORRECT";
             Answer.alpha = 1;
             StartCoroutine(TransitionAnswer());
@@ -92,6 +102,15 @@ public class GameManager : MonoBehaviour {
             else if (answerIndex == 2) { answerCircleText.enabled = false; circleButton.enabled = false; }
             else { answerSquareText.enabled = false; squareButton.enabled = false; }
         }
+    }
+
+    void CreateText() {
+        string path = Application.dataPath + "/Scores.txt";
+        if (!File.Exists(path)) {
+            File.WriteAllText(path, "");
+        }
+        File.AppendAllText(path, GameManager.questionLog);
+        File.AppendAllText(path, GameManager.Score.ToString() + " - " + System.DateTime.Now + "\n########################################################################\n");
     }
 
 }
