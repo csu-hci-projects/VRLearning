2d38c839c9e66c51dd32af7e64a959b3d124f71a
BigIslandGabe <gabep@rams.colostate.edu>
2020-04-24 23:26:16 -0600

    Finished 3D Lecture

--- unity/LogicLecture3D/Assets/Scripts/GateNode.cs
+++ unity/LogicLecture3D/Assets/Scripts/GateNode.cs
@@ -6,8 +6,9 @@ using Valve.VR.Extras;
 
 public class GateNode : MonoBehaviour {
 
+    public static GameObject down;
+    public static GameObject up;
     public GateNode node;
-    private GameObject clickedObject;
     public bool status;
     private LineRenderer line;
     public PointerEventArgs e = default(PointerEventArgs);
@@ -20,25 +21,42 @@ public class GateNode : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
-        if (line){
-            line.SetPosition(0, this.gameObject.transform.position);
-            line.SetPosition(1, clickedObject.transform.position);
+        if (line && node){
+            line.SetPosition(0, gameObject.transform.position);
+            line.SetPosition(1, node.transform.position);
         }
-        if (node){
-            node.status = status;
+        if (node) {
+            status = node.status;
+        }
+        if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.Any) && e.bHit)
+        {
+            down = e.target.gameObject;
         }
         if (SteamVR_Actions.default_GrabPinch.GetStateUp(SteamVR_Input_Sources.Any) && e.bHit)
         {
-            clickedObject = e.target.gameObject;
-            node = clickedObject.GetComponent<GateNode>();
-            if (line == null)
+            up = e.target.gameObject;
+            if (up && down)
             {
-                line = this.gameObject.AddComponent<LineRenderer>();
-                line.SetWidth(0.1F, .1F);
-                line.SetVertexCount(2);
-                line.material.color = Color.white;
+                if (down.GetInstanceID() == up.GetInstanceID()) status = !status;
+                else if (down.tag != "InputNode")
+                {
+                    node = down.GetComponent<GateNode>();
+                    if (line == null)
+                    {
+                        line = this.gameObject.AddComponent<LineRenderer>();
+                        line.SetWidth(0.1F, .1F);
+                        line.SetVertexCount(2);
+                        line.material.color = Color.white;
+                    }
+                    else
+                    {
+                        Destroy(gameObject.GetComponent<LineRenderer>());
+                        node = null;
+                    }
+                    down = null;
+                    up = null;
+                }
             }
-            status = !status;
         }
     }
 }
